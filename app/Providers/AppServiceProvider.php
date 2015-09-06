c74bf35e0ec7dfdfcf658eb87e661c029ca0c5c0
Colin DeCarlo <colin@thedecarlos.ca>
2015-09-06 09:03:01 -0400

    Setup the meetup provider in the AppServiceProviders boot method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace GPUG\Providers;
 
+use GuzzleHttp\Client as HttpClient;
+use SocialNorm\Meetup\MeetupProvider;
 use Illuminate\Support\ServiceProvider;
+use SocialNorm\Request as SocialNormRequest;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // bind and register the SocialNorm MeetupProvider with EloquentOAuth
+        $this->app->bind(MeetupProvider::class, function ($app) {
+            return new MeetupProvider (
+                $app['config']->get('eloquent-oauth.providers')['meetup'],
+                new HttpClient(),
+                new SocialNormRequest($app['request']->all())
+            );
+        });
+
+        $this->app['adamwathan.oauth']->registerProvider('meetup', $this->app[MeetupProvider::class]);
     }
 
     /**
