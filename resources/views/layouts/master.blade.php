22f572616e8f60a2fd26dc911a0c04f61f034202
Colin DeCarlo <colin@thedecarlos.ca>
2015-09-08 22:25:43 -0400

    Make the navbar bigger. Add links to sign in and sign out

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,14 +24,22 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">Guelph PHP User Group</a>
+          <a class="navbar-brand brand-logo" href="/">Guelph PHP User Group</a>
         </div>
+        @if (! Auth::check())
+            <a class="btn btn-primary navbar-btn navbar-right" href="/auth/login">Sign in with Meetup</a>
+        @else
+            <ul class="navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img id="user-menu" src="{{ Auth::user()->avatar }}" alt="Signed in as {{ Auth::user()->name }}" class="img-square img-profile"/>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="user-menu">
+                        <li><a href="/auth/logout">Sign out</a></li>
+                    </ul>
+                </li>
+            <ul>
+        @endif
       </div>
     </nav>
 
