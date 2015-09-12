74c266059fd2130dacda0602de970c52078c9c06
Colin DeCarlo <colin@thedecarlos.ca>
2015-09-12 15:43:44 -0400

    Use 'body' because fuck guzzle 5

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -15,7 +15,7 @@ class SlackController extends Controller
         $client = new Client(['http_errors' => false]);
         $client->post(
             'http://localhost:8080',
-            ['form_params' => ['email' => $request->input('email')]]
+            ['body' => ['email' => $request->input('email')]]
         );
 
         return redirect('/slack-invited');
