5e8f167933ddb6e5407b302b11f73e4accc85731
Colin DeCarlo <colin@thedecarlos.ca>
2015-09-12 15:52:46 -0400

    Make the invited text white.

--- resources/views/slack/invited.blade.php
+++ resources/views/slack/invited.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class"row">
-    <div class="col-md-12 text-center">
+    <div class="col-md-12 text-center white">
         <h1>Much Success!</h1>
         <p>Go check your email for an invite!</p>
     </div>
